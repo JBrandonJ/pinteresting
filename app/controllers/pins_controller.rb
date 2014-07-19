501bd56bd6cd94fe6d3cab4aa616e5757d0503f4
Brandon Johnson <jbrandonj@gmail.com>
2014-07-19 14:43:35 -0700

    added paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,6 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  
 
   def index
     @pins = Pin.all
@@ -39,6 +40,7 @@ class PinsController < ApplicationController
     redirect_to pins_url
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
@@ -53,6 +55,7 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   #Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  #Required for ImageMagick
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
